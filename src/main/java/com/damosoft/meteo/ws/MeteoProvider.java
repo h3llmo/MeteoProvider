a9f27a1f22355a2a1bc294cc11fbab8c245814a2
h3llmo <h3llmo@gmail.com>
2016-06-05 14:50:13 +0200

    - mockup ready

--- src/main/java/com/damosoft/meteo/ws/MeteoProvider.java
+++ src/main/java/com/damosoft/meteo/ws/MeteoProvider.java
@@ -1,16 +1,22 @@
 package com.damosoft.meteo.ws;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 
-/**
- * Created by User on 05/06/2016.
- */
-@ApplicationPath("meteo")
-@Path("services")
-public class MeteoProvider {
+@Path("/MyRestService")
+@ApplicationPath("/resources")
+public class MeteoProvider extends Application{
+    @Path("/sayHello")
+    @GET
+    public String getHello() {
+        return "hello world";
+    }
+
+    @GET
     @Path("{location}")
     public Response getMeteo(@PathParam("location") final String location) {
         return Response.ok("25.0").build();
